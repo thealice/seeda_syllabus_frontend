53a43adcc6ce3b0cf7c9b13c82e6437a2ffce562
Alice Freda <alicefreda@gmail.com>
2020-06-01 20:00:13 -0700

    get syllabus, create form handler

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const endPoint = "http://localhost:3000/api/v1/syllabuses";
 document.addEventListener('DOMContentLoaded', () => {
     getSyllabi();
+
+    const createSyllabusForm = document.querySelector('#create-syllabus-form');
+
+    createSyllabusForm.addEventListener('submit', (e) =>  createFormHandler(e))
 });
 
 function getSyllabi() {
@@ -24,3 +28,15 @@ function getSyllabi() {
       })
     //setup catch here for error reporting
 }
+
+function createFormHandler(e) {
+    e.preventDefault();
+    const titleInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const imageInput = document.querySelector('#input-url').value
+    const categoryInput = document.querySelector('#categories').value
+    const categoryId = parseInt(categoryInput)
+    postFetch(titleInput, descriptionInput, imageInput, categoryId);
+}
+
+function postFetch(title, description, image_url, category_id)
